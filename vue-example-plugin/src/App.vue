695cb516104bf9fc44e0953876ab36eb2cb83046
Jonathan KPEYI <58687833+jkpeyi@users.noreply.github.com>
2024-11-22 15:14:16 +0100

    chore: replace vue-signal by ref
    * chore: replace vue-signal by ref
    
    * Update package.json

--- vue-example-plugin/src/App.vue
+++ vue-example-plugin/src/App.vue
@@ -1,26 +1,25 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { signal } from 'vue-signals';
+import { onMounted, ref } from "vue";
 
-const theme = signal<string | null>(null);
+const theme = ref<string | null>(null);
 
 onMounted(() => {
   const url = new URL(window.location.href);
 
-  const initialTheme = url.searchParams.get('theme');
+  const initialTheme = url.searchParams.get("theme");
 
   if (initialTheme) {
-    theme.set(initialTheme as string);
+    theme.value = initialTheme as string;
   }
 
-  window.addEventListener('message', (event) => {
-    if (event.data.type === 'theme') {
-      theme.set(event.data.content);
+  window.addEventListener("message", (event) => {
+    if (event.data.type === "theme") {
+      theme.value = event.data.content;
     }
   });
 });
 </script>
 
 <template>
-  <main :data-theme="theme()">Welcome to your plugin!</main>
+  <main :data-theme="theme">Welcome to your plugin!</main>
 </template>
